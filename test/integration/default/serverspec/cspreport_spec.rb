7839081c00061d41658fa9a7f31382b2cee42e7b
juju4 <juju4@users.noreply.github.com>
2017-12-09 17:35:58 -0500

    kitchen/serverspec tests: re-align with config

--- test/integration/default/serverspec/cspreport_spec.rb
+++ test/integration/default/serverspec/cspreport_spec.rb
@@ -7,13 +7,15 @@ describe command('curl -kv https://localhost/csp/report.php'), :if => os[:family
   its(:stderr) { should match /HTTP\/1.1 204 No Content/ }
   its(:stderr) { should_not match /HTTP\/1.0 500 Internal Server Error/ }
   its(:stderr) { should match /SSL connection using TLS1.2/ }
-  its(:stderr) { should match /Server: Apache$/ }
+  its(:stderr) { should match /Server: Apache/ }
+  its(:stderr) { should match /Server: Apache\s*$/ }
 end
 
 describe command('curl -kv https://localhost/csp/report.php'), :if => os[:family] == 'ubuntu' && os[:release] == '14.04' do
   its(:stderr) { should match /HTTP\/1.1 204 No Content/ }
   its(:stderr) { should_not match /HTTP\/1.0 500 Internal Server Error/ }
   its(:stderr) { should match /SSL connection using ECDHE-RSA-AES256-GCM-SHA384/ }
-  its(:stderr) { should match /Server: Apache$/ }
+  its(:stderr) { should match /Server: Apache/ }
+  its(:stderr) { should match /Server: Apache\s*$/ }
   its(:stderr) { should_not match /-Powered-By: PHP\/5.5.9-1ubuntu4.22/ }
 end
